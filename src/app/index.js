e5de39c6514e06c9a5174e32e6d94221ba63a751
David Dias <daviddias.p@gmail.com>
2016-12-13 12:12:06 -0800

    add a simple welcome page to visualize the fetch of an hash

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,25 @@
 'use strict'
 
 const IPFS = require('./ipfs')
-const visualizer = require('./visualizer')
+// const visualizer = require('./visualizer')
+const concat = require('concat-stream')
 
 document.addEventListener('DOMContentLoaded', () => {
   IPFS.create((err, node) => {
     if (err) { throw err }
 
     document.getElementById('visualize')
-      .onclick = visualizer(node)
+      .onclick = () => { // visualizer(node)
+        const hash = document.getElementById('source').value
+        node.files.cat(hash, function (err, res) {
+          if (err || !res) {
+            return console.error('ipfs cat error', err, res)
+          }
+
+          res.pipe(concat(function (data) {
+            document.getElementById('content').innerText = data
+          }))
+        })
+      }
   })
 })
